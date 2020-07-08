45f433b661c2b93ff5738efa48e49268e6980cad
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-07-08 14:19:27 +0200

    Delete uptime.js

--- uptime.js
+++
@@ -1,7 +0,0 @@
-const discord = require("discord.js")
-const moment = require("moment");
-exports.run = async (bot , message , args) => {
-    require("moment-duration-format");
-    const uptime = moment.duration(bot.uptime).format(" D [Tage], H [Stunden], m [Minuten], s [Sekunden]");
-    message.channel.send(uptime)
-}
