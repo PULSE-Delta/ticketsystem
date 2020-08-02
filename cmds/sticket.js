efe66abc377e6ade3cc71cfb1cc1ef3ef1c46d78
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-03 00:46:59 +0200

    Update sticket.js

--- cmds/sticket.js
+++ cmds/sticket.js
@@ -1,12 +1,11 @@
 const discord = require("discord.js")
 exports.run = async (bot , message , args) => {   
       const guild = message.guild;
-    const role = guild.roles.find(r => r.name === "Supporter");
-  if (!role) {
-      return message.reply("Das Ticket kann nur durch einen Supporter geschlossen werden");
-    }
+  if (!message.member.hasPermission ("ADMINISTRATOR"))
+      return message.reply("Du benötigst die Berechtigung: ADMINISTRATOR");
+    
     const usechannel = message.mentions.channels.first();
   if (!usechannel) return message.reply ("Gebe den Ticketnamen an!")
-    bot.channels.get(usechannel.id).delete(5000);
-    message.channel.send("Geschlossen!");
+    bot.channels.get(usechannel.id).delete(10000);
+    message.reply(`${usechannel} wurde Geschlossen!`);
   }
