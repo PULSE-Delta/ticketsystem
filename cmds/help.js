3872665dbea3b28d7714d1869cbce533259b81de
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-03 00:38:13 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -2,7 +2,13 @@ const discord = require("discord.js")
 exports.run = async(bot , message , args) => {
 emb = new discord.RichEmbed();
 .setTitle("Help")
-.setName("Test")
+.setDescription("Du hast den Hilfe Command aufgerufen")
 .setColor("BLUE")
+.addField("freunde" , "Zeigt dir andere Bots")
+.addField("invite" , "So kannst du mich einladen ^.^")
+.addField("ticket" , "öffnet ein Ticket!")
+.addField("ADMIN COMMANDS" , "-------------------------")
+.addField("sticket #ticket-name" , "Schließt das Ticket wieder") 
+.setFooter(`${message.author.tag}` , message.author.avatarURL)
 message.reply(emb)
 }
